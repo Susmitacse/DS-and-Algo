b133330e5246370a6be504ed319933a058f7bf91
SUSMITA DAS <47671769+Susmitacse@users.noreply.github.com>
2021-08-17 12:27:36 +0530

    Update and rename 1_array.cpp to 1.array.cpp

--- 1.array.cpp
+++ 1.array.cpp
@@ -1,3 +1,5 @@
+// When an array is passed to the function it is passed by reference. (Only the address is passed)
+// If we need to pass an array to a function we also need to pass the size along with the reference to the array, otherwise it wont know upto what extent the array is 
 
 #include <iostream>
 using namespace std;
@@ -8,7 +10,7 @@ void printArray(int arr[])
 }
 
 int main() {
-	// your code goes here
+	
     int arr[] = {1,2,3,4,5,6};
     int n = sizeof(arr)/sizeof(int);
     cout <<"inside main" << sizeof(arr) <<endl;
