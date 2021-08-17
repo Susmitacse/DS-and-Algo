31cf62acc30e8bfbe1b5a1d024d40a3eb65b7942
SUSMITA DAS <47671769+Susmitacse@users.noreply.github.com>
2021-08-17 12:32:20 +0530

    Update 1.array.cpp

--- 1.array.cpp
+++ 1.array.cpp
@@ -1,5 +1,6 @@
 // When an array is passed to the function it is passed by reference. (Only the address is passed)
-// If we need to pass an array to a function we also need to pass the size along with the reference to the array, otherwise it wont know upto what extent the array is 
+// If we need to pass an array to a function we also need to pass the size along with the reference to the array, otherwise it wont know upto what extent the array is
+// In main the size it prints is the size of the array 6x4(each int size)= 24 and inside function it prints 8 (the size of address of a reference variable)
 
 #include <iostream>
 using namespace std;
